cd06e8997e6c1e6c8bad967af90e14290b18d438
Azamat <33450014+MlAzamat@users.noreply.github.com>
2025-01-26 10:11:24 +0300

    1

--- mock_json/src/main/java/ru/server/mock/MockApplication.java
+++ mock_json/src/main/java/ru/server/mock/MockApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-	public class MockApplication {
+public class MockApplication {
 
-		public static void main(String[] args) {
-			SpringApplication.run(MockApplication.class, args);
-		}
-
+	public static void main(String[] args) {
+		SpringApplication.run(MockApplication.class, args);
 	}
+
+}
